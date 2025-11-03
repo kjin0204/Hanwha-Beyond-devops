84a90324d6dff4fafd8b78be5acec6dacd71fb05
HyeonggyuKang <kin0204@naver.com>
2025-11-03 11:08:21 +0900

    fron, back 각각의 컨테이너로 분리 후 통신 예제

--- chap01_01_bootproject/src/main/java/com/hyeong/bootproject/config/WebConfig.java
+++ chap01_01_bootproject/src/main/java/com/hyeong/bootproject/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")                  // origin 이후 요청 경로 패턴
                 .allowedOrigins("*")        // origin 등록(어떤 front 서버든 상관 없이 허용)
                 .allowedOrigins("http://localhost:5173")        // origin 등록
+                .allowedOrigins("http://localhost:8011")        // origin 등록(프론트가 컨테이너화 된 이후)
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
