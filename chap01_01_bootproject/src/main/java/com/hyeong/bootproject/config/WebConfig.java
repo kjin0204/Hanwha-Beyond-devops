c816ac28610d950bac1d20c494d860972c754ebb
HyeonggyuKang <kin0204@naver.com>
2025-11-05 10:34:54 +0900

    쿠버네티스 설정 후 실행 테스트 진행
    1. nodeport 설정으로 외부에서 바로 worker node로 접근 가능
    2. 프론트, 백엔드 모두 worker node의 port 기준으로 접근하도록 설정

--- chap01_01_bootproject/src/main/java/com/hyeong/bootproject/config/WebConfig.java
+++ chap01_01_bootproject/src/main/java/com/hyeong/bootproject/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 
@@ -13,8 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")                  // origin 이후 요청 경로 패턴
                 .allowedOrigins("*")        // origin 등록(어떤 front 서버든 상관 없이 허용)
-                .allowedOrigins("http://localhost:5173")        // origin 등록
-                .allowedOrigins("http://localhost:8011")        // origin 등록(프론트가 컨테이너화 된 이후)
+                // .allowedOrigins("http://localhost:5173")        // origin 등록
+                // .allowedOrigins("http://localhost:8011")        // origin 등록(프론트가 컨테이너화 된 이후)
+                .allowedOrigins("http://localhost:30000")        // origin 등록(k8s nodeport 이후)
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
--- chap01_02_vueproject/src/App.vue
+++ chap01_02_vueproject/src/App.vue
@@ -43,8 +43,17 @@
     //       body: JSON.stringify({num1: num1.value, num2: num2.value})
     //     })
 
-    /* 5. 백엔드에서 x, 프론트에서 CORS(프론트와 백엔드 모두 컨테이너화(docker-copose, bridge network활용)) */
-    const response = await fetch(`http://localhost:8011/api/plus`, {
+    // /* 5. 백엔드에서 x, 프론트에서 CORS(프론트와 백엔드 모두 컨테이너화(docker-copose, bridge network활용)) */
+    // const response = await fetch(`http://localhost:8011/api/plus`, {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json;charset=utf-8;'
+    //       },
+    //       body: JSON.stringify({num1: num1.value, num2: num2.value})
+    //     })
+
+    /* 6. 백엔드에서 CORS, 프론트에서 x(k8s nodeport 적용(인그래스 사용 전) ) */
+    const response = await fetch(`http://localhost:30001/plus`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json;charset=utf-8;'
